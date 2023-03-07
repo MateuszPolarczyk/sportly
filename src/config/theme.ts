dc44c4f2aecf7565f6a7396bf77806de31ed3ecd
MateuszPolarczyk <mateusz.polarczyk@interia.pl>
2023-03-07 22:41:25 +0100

    feat: global theme settings

---
+++ src/config/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+    colors: {
+        primary: "#598151",
+        secondary: "#FFC857",
+        tertiary: "#F18F01",
+        accent: "#91BC3A",
+        dark: "#2B2D42",
+        light: "#EDF2F4",
+    },
+    typography: {
+        heading1: 40,
+        heading2: 32,
+        heading3: 20,
+        paragraph: 18,
+        button: 15
+    },
+    iconSize: {
+        small: 10,
+        medium: 15,
+        large: 20,
+        xlarge: 30
+    }
+}
+
+export { theme };
