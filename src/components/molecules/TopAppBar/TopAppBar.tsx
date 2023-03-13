3598c5570fc4a3f37d45f20d734767d0ecaa51f8
MateuszPolarczyk <mateusz.polarczyk@interia.pl>
2023-03-13 22:20:15 +0100

    fix: back button function with new styling, left item props removed

--- src/components/molecules/TopAppBar/TopAppBar.tsx
+++ src/components/molecules/TopAppBar/TopAppBar.tsx
@@ -1,18 +1,29 @@
 import { ReactNode } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 import styled from "styled-components/native";
 import { ViewStyle } from "react-native";
 import { Paragraph } from "../../atoms/Paragraph/Paragraph";
+import { Icon } from "../../atoms/Icon/Icon";
 
 const TopAppBarContainer = styled.View`
   flex-direction: row;
   padding: 10px;
 `;
 
-const LeftItemContainer = styled.TouchableOpacity`
+const LeftItemContainer = styled.View`
   flex: 0.2;
   alignitems: flex-start;
-  margin-left: 10px;
+  margin-left: 5px;
+`;
+
+const BackButtonContainer = styled.TouchableOpacity`
+  width: 50px;
+  heigth: 50px;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(211, 211, 211, 0.7);
 `;
 
 const TitleContainer = styled.View`
@@ -28,23 +39,33 @@ const RightItemContainer = styled.TouchableOpacity`
 interface TopAppBarProps {
   title?: string;
   rightItem?: ReactNode;
-  leftItem?: ReactNode;
+  backButton?: boolean;
   containerStyle?: ViewStyle;
-  onLeftClick?: () => void;
   onRightClick?: () => void;
 }
 
 const TopAppBar = ({
   title,
   rightItem,
-  leftItem,
   containerStyle,
-  onLeftClick,
   onRightClick,
+  backButton,
 }: TopAppBarProps) => {
+  const navigation = useNavigation();
+
   return (
     <TopAppBarContainer style={containerStyle}>
-      <LeftItemContainer onPress={onLeftClick}>{leftItem}</LeftItemContainer>
+      <LeftItemContainer>
+        {backButton && (
+          <BackButtonContainer
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Icon name="chevron-back" size={40} color="#232829" />
+          </BackButtonContainer>
+        )}
+      </LeftItemContainer>
       <TitleContainer>
         <Paragraph>{title}</Paragraph>
       </TitleContainer>
