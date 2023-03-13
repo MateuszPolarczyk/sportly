e6fa955e8e0ce394b975f26fd12debaca71f1e5d
MateuszPolarczyk <mateusz.polarczyk@interia.pl>
2023-03-13 23:39:28 +0100

    feat: Booking form and payment form boilerplate

---
+++ src/views/Booking/Form.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+interface P {}
+
+const BookingFormScreen = ({}: P) => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export { BookingFormScreen };
---
+++ src/views/Booking/Payment.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+interface P {}
+
+const PaymentFormScreen = ({}: P) => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export { PaymentFormScreen };
